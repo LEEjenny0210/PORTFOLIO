1cea6db1a3ab6b0d0782d299b9ba2cbd712fc210
jenny <rhdwnsla410@naver.com>
2023-03-29 17:57:42 +0900

    d

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
 
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
         mapOption = {
-            center: new kakao.maps.LatLng(36.3025900589377, 127.3470985352889), // 지도의 중심좌표
+            center: new kakao.maps.LatLng(36.3025900589377, 127.3470985352889), // 지도의 중심좌표 
             level: 3 // 지도의 확대 레벨
         };
 
